d8457da8d7444d8f548a591719ed476e5e8e7688
RudeGalaxy1010 <Karev007@bk.ru>
2023-05-11 13:31:14 +0300

    Add value text to stats display

--- Assets/Source/Menu/Skin Customization/SkinStatsDisplay.cs
+++ Assets/Source/Menu/Skin Customization/SkinStatsDisplay.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +8,9 @@ public class SkinStatsDisplay : MonoBehaviour
     [SerializeField] private Slider _forceSlider;
     [SerializeField] private Slider _accuracySlider;
     [SerializeField] private Slider _luckSlider;
+    [SerializeField] private TMP_Text _forceText;
+    [SerializeField] private TMP_Text _accuracyText;
+    [SerializeField] private TMP_Text _luckText;
 
     private Skin _skin;
 
@@ -33,5 +37,30 @@ public class SkinStatsDisplay : MonoBehaviour
         _forceSlider.value = force;
         _accuracySlider.value = accuracy;
         _luckSlider.value = luck;
+
+        _forceText.text = GetFormattedStat(force);
+        _accuracyText.text = GetFormattedStat(accuracy);
+        _luckText.text = GetFormattedStat(luck);
+    }
+
+    private string GetFormattedStat(float value)
+    {
+        float roundedValue = Mathf.RoundToInt(value * 100) / 10f;
+        return $"{GetSign(roundedValue)} {Mathf.Abs(roundedValue)}";
+    }
+
+    public string GetSign(float value)
+    {
+        if (value > 0)
+        {
+            return "+";
+        }
+
+        if (value < 0)
+        {
+            return "-";
+        }
+
+        return "";
     }
 }
